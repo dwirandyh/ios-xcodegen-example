9851a69dbb5f0c6c39fd2a9212e77d0cfa10e731
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2021-07-17 20:37:37 +0700

    Simple project and Test with xcode

---
+++ XcodegenExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcodegen-example-app
+//
+//  Created by Dwi Randy Herdinanto on 17/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
